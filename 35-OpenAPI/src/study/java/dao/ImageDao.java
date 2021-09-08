2a6d39f7b497052bfd04c09eb7150748427d7cce
inhopark <ebusinho@gmail.com>
2021-09-08 22:47:55 +0900

    35-OpenAPI

---
+++ 35-OpenAPI/src/study/java/dao/ImageDao.java
@@ -0,0 +1,9 @@
+package study.java.dao;
+
+import java.util.List;
+
+import study.java.model.ImageItem;
+
+public interface ImageDao {
+	public List<ImageItem> getImageSearchList(String keyword);
+}
